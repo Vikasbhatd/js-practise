b6b11c03238cc04cfae1e3ba1577f3e532892775
vikas-bhat-d <139564925+vikas-bhat-d@users.noreply.github.com>
2024-01-19 14:21:58 +0000

    done

--- Basics/02_DataTypes.js
+++ Basics/02_DataTypes.js
@@ -4,3 +4,10 @@ console.log(typeof counter)
 let counter2="a"
 let CounterInNumber=Number(counter2)
 console.log(CounterInNumber,"\n",typeof CounterInNumber)
+
+/*
+Primitive: String , Number, Boolean , null, undefined, Symbol, BigInt
+
+Reference(Non-reference): Array , Objects , Functions
+
+*/
