f93a39678bb349a8c09246ee9f97730186d03499
Vikas Bhat D <vikasdbhat@gmail.com>
2024-01-21 16:16:23 +0000

    strings done

--- Basics/04_Strings.js
+++ Basics/04_Strings.js
@@ -1,3 +1,16 @@
 let name="vikas"
 let score=0
 console.log(`My name is ${name} and score is ${score}`)
+
+let myName=new String('Vikas')
+console.log(myName[2])
+console.log(myName.length)
+console.log(myName.charAt(2))
+console.log(myName.indexOf('k'));
+console.log(myName.toLocaleUpperCase())
+const newString=myName.slice(-3,4)
+console.log(newString);
+const newStringOne="    vikas  "
+console.log(newStringOne.trim()) //trims the whitespaces from beginning and end
+const url="https://google.com/vikas%20bhat"
+console.log(url.replace('%20','_'))
