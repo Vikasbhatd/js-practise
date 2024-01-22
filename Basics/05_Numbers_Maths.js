6259c934de3cbb0957527ab3c2c3d312116e1103
Vikas Bhat D <vikasdbhat@gmail.com>
2024-01-22 06:35:44 +0000

    numbers and math done

---
+++ Basics/05_Numbers_Maths.js
@@ -0,0 +1,17 @@
+//************** Numbers ***********
+// const num= new Number('1000.734')
+// console.log(num)
+// console.log(num.toFixed(2))
+// console.log(num.toString())
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(Number.MIN_SAFE_INTEGER)
+
+//****************Maths***********/
+
+// console.log(Math.abs(-4))
+// console.log(Math.PI)
+console.log(Math.floor((Math.random()*10))+1)
+
+const min=15
+const max=25
+console.log(Math.floor(Math.random()*(max-min+1))+min)
